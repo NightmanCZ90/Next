1c7c5cce307637d394a8a76a249c546cba6bd16b
NightmanCZ90 <novak.jan90@gmail.com>
2020-09-26 12:22:06 +0200

    Next.js basics

---
+++ first-next/pages/about.js
@@ -0,0 +1,9 @@
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>This Is the about page</h1>
+    </div>
+  )
+}
+export default AboutPage;
---
+++ first-next/pages/contact.js
@@ -0,0 +1,10 @@
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>Do not contact us ever</p>
+    </div>
+  )
+}
+export default Contact;
