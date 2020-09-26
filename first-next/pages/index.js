6cb065e6e4a14bfc27ea329243b64f04c425da02
NightmanCZ90 <novak.jan90@gmail.com>
2020-09-26 14:00:12 +0200

    intro to getInitialProps

--- first-next/pages/index.js
+++ first-next/pages/index.js
@@ -1,11 +1,33 @@
+import React, { Component } from 'react';
 
+class Index extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       
+    }
+  }
+  
+  static async getInitialProps() {
+    console.log("Fetching your data in getInitialProps");
+  }
+  
+  render() {
+    return (
+      <div>
+        <h1>Our Index Page</h1>
+      </div>
+    )
+  }
+}
 
-const Index = () => {
-  return (
-    <div>
-      <h1>Our Index Page</h1>
-    </div>
-  )
-};
+// const Index = () => {
+//   return (
+//     <div>
+//       <h1>Our Index Page</h1>
+//     </div>
+//   )
+// };
 
 export default Index;
